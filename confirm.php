3ab7595041f16880a57b9d861580e028fd7e18b1
mizuno <31834@toyota.kosen-ac.jp>
2022-04-26 10:47:04 +0900

    Add: confirm page

---
+++ confirm.php
@@ -0,0 +1,28 @@
+<?php require('data.php'); ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Café Progate</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <div class="order-wrapper">
+        <h2>注文内容確認</h2>
+        <?php foreach ($menus as $menu) : ?>
+            <?php $orderCount = $_POST[$menu->getName()] ?>
+            <p class="order-amount">
+                <?php echo $menu->getName() ?>
+                x
+                <?php echo $orderCount ?>
+                個
+            </p>
+        <?php endforeach ?>
+    </div>
+</body>
+
+</html>
